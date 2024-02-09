da31d5d3b8cdf8430e3f7cb02c9b18324dc4b610
Oriana Benitez <orianabenitez6227@gmail.com>
2024-02-09 15:28:30 -0400

    inversion 2nd

--- ert_inversion.py
+++ ert_inversion.py
@@ -1,13 +1,29 @@
 import pygimli as pg
 from pygimli.physics import ert
 import matplotlib.pyplot as plt
+from matplotlib import colormaps
 
-data = ert.load('data\merged.txt')
-#print(data)
+data = ert.load('data\mergedtest.txt')
+print(data)
 
-fig, ax = plt.subplots()
-ax.set_xlim(xmin=1,xmax=(max(pg.x(data)+1)))
-ax.set_ylim(ymin=2835,ymax=(max(pg.z(data))+1))
-ax.plot(pg.x(data), pg.z(data),'.')
-#ax.set_aspect(1)
-plt.show()
+# fig, ax = plt.subplots()
+# ax.plot(pg.x(data), pg.z(data),'.')
+# ax.set_aspect(1)
+# plt.show()
+
+k_num = ert.createGeometricFactors(data, numerical=True)
+k_ana = ert.createGeometricFactors(data, numerical=False)
+
+# ert.show(data, vals=k_num/k_ana, label='Efecto de topografía',
+#          cMap="plasma",logscale=True)
+
+data['k']=k_num
+data['err'] = ert.estimateError(data,absoluteError=5)
+#ert.show(data)
+
+mgr = ert.ERTManager(data)
+# mgr.invert(data, lam=10, verbose = True,quality=30, paraDepth =20)
+# mgr.showResult(cMap='verbose')
+mod = mgr.invert(data, lam=25, verbose=True, quality=35)
+
+mgr.showResult(cMap='viridis')
