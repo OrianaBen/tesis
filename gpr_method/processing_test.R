40b81539f4fe07735c0a4ef9bbf7e35d1d309f51
Oriana Benitez <orianabenitez6227@gmail.com>
2024-07-07 18:07:04 -0400

    notebooks and R order

--- gpr_method/processing_test.R
+++ gpr_method/processing_test.R
@@ -8,14 +8,14 @@ x <- readGPR(dsn = "rawGPR/7usbPER.ZON/TF510001.dt", Vmax = NULL,
 
 antfreq(x) <- 200
 
-png("7usb/rawdata.png")
+png("test/rawdata.png")
 plot(x, col=palGPR("gray"))
 dev.off()
 
 #estimacion de primera llegada y t0, y grafica para cada traza
 time0(x)
 
-png("7usb/firstbreak.png")
+png("test/firstbreak.png")
 tfb <- firstBreak(x, w = 0.5, method = "MER")
 plot(pos(x), tfb, ylab = "first wave break", xlab = "position (m)")
 dev.off()
@@ -23,57 +23,52 @@ dev.off()
 t0 <- firstBreakToTime0(tfb, x)
 time0(x) <- mean(t0)
 
-png("7usb/timezero_firstb.png")
+png("test/timezero_firstb.png")
 plot(x[, 15], xlim = c(0, 50))
 abline(v = tfb[15], col = "blue")
 dev.off()
 
-# plot(x[, 15])  # plot the 15th trace of the GPR-line
-# # add a green horizontal line
-# abline(h = mean(x[1:100, 15]), col = "green")
-
 x1 <- dcshift(x, u = 1:120)
 proc(x1)
 
-png("7usb/timezerocorrec.png")
+png("test/timezerocorrec.png")
 x2 <- time0Cor(x1, method = "spline")
 plot(x2)
 dev.off()
 
-png("7usb/dewow.png")
+#dewow componente de baja frecuencia a partir de mediana
+png("test/dewow.png")
 x3 <- dewow(x2, type = c("runmean"), w = 5)
 plot(x3)
 dev.off()
 
-png("7usb/removedwow.png")
+png("test/removedwow.png")
 plot(x3 - x2)
 dev.off()
 
-png("7usb/comparisonbeforeafter.png")
-plot(x2[,15], col = "blue")      # before dewowing
-lines(x3[,15], col = "red")      # after dewowing
+#comparacion amplitud vs tiempo de viaje antes y despues dewow
+png("test/comparisonbeforeafter.png")
+plot(x2[,15], col = "blue")      
+lines(x3[,15], col = "red")
 dev.off()
 
-png("7usb/freqspec.png")
+png("test/freqspec.png")
 spec1D(x3)
 dev.off()
 
-png("7usb/lowpassspec.png")
+png("test/lowpassspec.png")
 x4 <- fFilter(x3, f =1000,
               type = "low", plotSpec = TRUE)
 dev.off()
 
-png("7usb/withoutnoise.png")
+png("test/withoutnoise.png")
 plot(x4, col=palGPR("gray"))
 dev.off()
 
-# # compute the trace amplitude envelopes (with Hilbert transform)
+# calcular envolventes de trazas con transformada de Hilbert
 x4_env <- envelope(x4)
-# # plot all the trace amplitude envelopes as a function of time
-# trPlot(x4_env, log = "y", col = rgb(0.2,0.2,0.2,7/100), xlim = c(0, 250))
-# # plot the log average amplitude envelope
-# lines(traceStat(x4_env), log = "y", col = "red", lwd = 2)
 
+#aplicar ganancia
 x5 <- gainSEC(
   x4,
   a = 0.01,
@@ -84,17 +79,20 @@ x5 <- gainSEC(
   track = TRUE
 )
 
-#trace amplitude envelopes (with Hilbert transform)
-png("7usb/traceamplitudeenvs.png")
+#guardar grafica de envolvente de trazas y perfil con ganancia
+png("test/traceamplitudeenvs.png")
 plot(traceStat(x4_env), log = "y", col = "red", lwd = 2)
 lines(traceStat(envelope(x5)), log = "y", col = "green", lwd = 2)
 dev.off()
 
-png("7usb/gainapplied.png")
-plot(x5, col = palGPR("hcl-7"))
+png("test/result.png", width = 1500, height = 500, units = "px")
+plot(x5, col = palGPR("hcl-7"), ylim = c(1, 80), asp = 0.1)
 dev.off()
 
 proc(x5)
+
+writeGPR(x, fPath = "test_crudo", type = c("ASCII"))
+writeGPR(x5, fPath = "test_procesada", type = c("ASCII"))
 
 #1usb tcst = 1 tend = 90 wow c = runmean w = 0.05
 #2usb tcst = 5 tend = 40 c = runmean w = 0.05
