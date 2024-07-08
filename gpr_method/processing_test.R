a89cc5e3f20e3604cb99a6e2771234cd2d088e53
Oriana Benitez <orianabenitez6227@gmail.com>
2024-07-08 19:36:18 -0400

    myplot is my spanish (and other things) version for plotting gpr

--- gpr_method/processing_test.R
+++ gpr_method/processing_test.R
@@ -1,29 +1,33 @@
+Sys.setenv("LANGUAGE" = "Sp")
 library(RGPR)
 myDir <- "C:\\Users\\Oriana\\Uni\\Tesis\\tesis\\gpr_method"
 setwd(myDir)
+source("myplot.R")
 
 #leer y graficar data cruda
-x <- readGPR(dsn = "rawGPR/7usbPER.ZON/TF510001.dt", Vmax = NULL,
+x <- readGPR(dsn = "rawGPR/A2-A1.ZON/TF510001.dt", Vmax = NULL,
              method = c("spline", "spline", "spline"))
 
 antfreq(x) <- 200
 
-png("test/rawdata.png")
-plot(x, col=palGPR("gray"))
+png("A2-A1/rawdata.png", width = 950, height = 300, units = "px")
+plot(x, col = palGPR("gray"), ylim = c(0, 80), barscale = FALSE, clab = "",
+     addTime0 = FALSE, asp = 0.1, note = "", main = "", 
+     cex.lab = 1.5, cex.axis = 1.2)
 dev.off()
 
-#estimacion de primera llegada y t0, y grafica para cada traza
+# #estimacion de primera llegada y t0, y grafica para cada traza
 time0(x)
 
-png("test/firstbreak.png")
-tfb <- firstBreak(x, w = 0.5, method = "MER")
+png("A2-A1/firstbreak.png")
+tfb <- firstBreak(x, w = 0.2, method = "MER")
 plot(pos(x), tfb, ylab = "first wave break", xlab = "position (m)")
 dev.off()
 
 t0 <- firstBreakToTime0(tfb, x)
 time0(x) <- mean(t0)
 
-png("test/timezero_firstb.png")
+png("A2-A1/timezero_firstb.png")
 plot(x[, 15], xlim = c(0, 50))
 abline(v = tfb[15], col = "blue")
 dev.off()
@@ -31,37 +35,37 @@ dev.off()
 x1 <- dcshift(x, u = 1:120)
 proc(x1)
 
-png("test/timezerocorrec.png")
+png("A2-A1/timezerocorrec.png")
 x2 <- time0Cor(x1, method = "spline")
 plot(x2)
 dev.off()
 
 #dewow componente de baja frecuencia a partir de mediana
-png("test/dewow.png")
+png("A2-A1/dewow.png")
 x3 <- dewow(x2, type = c("runmean"), w = 5)
 plot(x3)
 dev.off()
 
-png("test/removedwow.png")
+png("A2-A1/removedwow.png")
 plot(x3 - x2)
 dev.off()
 
 #comparacion amplitud vs tiempo de viaje antes y despues dewow
-png("test/comparisonbeforeafter.png")
+png("A2-A1/comparisonbeforeafter.png")
 plot(x2[,15], col = "blue")      
 lines(x3[,15], col = "red")
 dev.off()
 
-png("test/freqspec.png")
+png("A2-A1/freqspec.png")
 spec1D(x3)
 dev.off()
 
-png("test/lowpassspec.png")
-x4 <- fFilter(x3, f =1000,
+png("A2-A1/lowpassspec.png")
+x4 <- fFilter(x3, f = 1000,
               type = "low", plotSpec = TRUE)
 dev.off()
 
-png("test/withoutnoise.png")
+png("A2-A1/withoutnoise.png")
 plot(x4, col=palGPR("gray"))
 dev.off()
 
@@ -74,30 +78,33 @@ x5 <- gainSEC(
   a = 0.01,
   b = 1,
   t0 = NULL,
-  tend = 100,
+  tend = 80,
   tcst = 2,
   track = TRUE
 )
 
 #guardar grafica de envolvente de trazas y perfil con ganancia
-png("test/traceamplitudeenvs.png")
+png("A2-A1/traceamplitudeenvs.png")
 plot(traceStat(x4_env), log = "y", col = "red", lwd = 2)
 lines(traceStat(envelope(x5)), log = "y", col = "green", lwd = 2)
 dev.off()
 
-png("test/result.png", width = 1500, height = 500, units = "px")
-plot(x5, col = palGPR("hcl-7"), ylim = c(1, 80), asp = 0.1)
+png("A2-A1/result.png", width = 950, height = 300, units = "px")
+plot(x5, col = palGPR("hcl-7"), ylim = c(0, 80), barscale = FALSE, clab = "",
+     addTime0 = FALSE, asp = 0.1, note = "", main = "A2-A1",
+     cex.lab = 1.5, cex.axis = 1.2)
+
 dev.off()
 
 proc(x5)
 
-writeGPR(x, fPath = "test_crudo", type = c("ASCII"))
-writeGPR(x5, fPath = "test_procesada", type = c("ASCII"))
+writeGPR(x, fPath = "A2-A1/raw_gpr", type = c("ASCII"), overwrite = TRUE)
+writeGPR(x5, fPath = "A2-A1/result_gpr", type = c("ASCII"), overwrite = TRUE)
 
-#1usb tcst = 1 tend = 90 wow c = runmean w = 0.05
-#2usb tcst = 5 tend = 40 c = runmean w = 0.05
-#3usb tcst = 2 tend = 70 wow c("gaussian"), w = 1
-#4usb tcst = 5 tend = 100 c = runmean w = 0.05
-#5usb tcst = 12 tend = 90 c = runmean w = 5
-#6usb tcst = 5 tend = 90 c = runmean w = 10
-#7usb tcst = 2 tend = 100 c = runmean w = 5
+#A tcst = 1 tend = 90 wow c = runmean w = 0.05
+#B tcst = 5 tend = 40 c = runmean w = 0.05
+#C tcst = 2 tend = 70 wow c("gaussian"), w = 1
+#D tcst = 5 tend = 100 c = runmean w = 0.05
+#E tcst = 12 tend = 90 c = runmean w = 5
+#F tcst = 5 tend = 90 c = runmean w = 10
+#G tcst = 2 tend = 100 c = runmean w = 5
